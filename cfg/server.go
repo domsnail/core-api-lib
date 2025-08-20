d5f10d7db3a3b160dcafc75e97d8b5d64cd45fd8
ydlysak <lysak.yaroslav00@yandex.ru>
2025-08-20 15:10:04 +0300

    added pkg health

--- cfg/server.go
+++ cfg/server.go
@@ -5,4 +5,5 @@ type ServerConfig struct {
 	Port uint64 `yaml:"port" env:"SRV_PORT" env-default:"6379" env-description:"Server listener port"`
 
 	Reflection bool `yaml:"reflection" env:"SRV_REFLECTION" env-default:"false" env-description:"Enables GRPC reflection"`
+	Health     bool `yaml:"health" env:"SRV_HEALTH" env-default:"true" env-description:"Enables GRPC health checks"`
 }
